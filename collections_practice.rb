ee000fdb378c10365d964eb37e99e922066e31a3
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-17 08:00:49 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -30,7 +30,7 @@ def merge_data(keys, data)
   data.each do |hash|
     hash.each do |k, v|
       keys.each do |kv_pair|
-        merged_hashes << kv_pair(value) if kv_pair.values[0] == value
+        merged_hashes << kv_pair(v) if kv_pair.values[0] == value
 end
 end
 end
