c080a5dd9e65185da5551a93a74d6e1b36277528
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-17 07:17:03 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,7 +18,8 @@ end
 def count_elements(array)
   counts = Hash.new(0)
   array.each do { |element| counts[element] += 1 }
-    count_array
+    count_array = []
+    count_array.each do ||
 end
 
 def merge_data(data)
