03e8618d825a693273ffb8bfdb6f0f376a92f4b6
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-17 07:15:46 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,33 @@
-# your code goes here
+def begins_with_r(array)
+  array.each { |word| return false if word.start_with?("r") == false }
+  true 
+end
+
+def contain_a(element)
+  element.select { |word| word.include?("a") }
+end
+
+def first_wa(element)
+  element.each { |word| return word if word.to_s.include?("wa") }
+end
+
+def remove_non_strings(array)
+  array.delete_if { |element| element.class != String }
+end
+
+def count_elements(array)
+  appearances = {}
+  array.c
+end
+
+def merge_data(data)
+  
+end
+
+def find_cool(hashes)
+
+end
+
+def organize_schools(schools)
+
+end
