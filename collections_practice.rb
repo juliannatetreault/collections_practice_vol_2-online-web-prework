353196aa9bba501b4398a241531243e4b8f9c708
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-17 07:47:05 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -26,7 +26,7 @@ final_count
 end
 
 def merge_data(data)
-  first_data[0].values.map.with_index { |key, value| }
+  first_data[0].values.map.with_index { |key, value| data_two }
 end
 
 def find_cool(hashes)
